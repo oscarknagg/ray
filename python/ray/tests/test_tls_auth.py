7b23f9e51f061b47bc2f94fc3bbea902d493c785
Oscar Knagg <oscar.knagg@gresearch.co.uk>
2021-10-12 14:54:13 +0100

    Fix tests

--- python/ray/tests/test_tls_auth.py
+++ python/ray/tests/test_tls_auth.py
@@ -10,20 +10,6 @@ import ray
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.skipif(
-    sys.platform == "darwin",
-    reason=("Cryptography doesn't install in Mac build pipeline"))
-@pytest.mark.parametrize("use_tls", [True], indirect=True)
-def test_client_connect_to_tls_server(use_tls, init_and_serve):
-    from ray.util.client import ray
-    os.environ["RAY_USE_TLS"] = "0"
-    with pytest.raises(ConnectionError):
-        ray.connect("localhost:50051")
-
-    os.environ["RAY_USE_TLS"] = "1"
-    ray.connect("localhost:50051")
-
-
 @pytest.mark.parametrize("use_tls", [True], indirect=True)
 def test_put_get_with_tls(shutdown_only, use_tls):
     ray.init(num_cpus=0)
@@ -54,8 +40,8 @@ def test_put_get_with_tls(shutdown_only, use_tls):
 
 
 @pytest.mark.parametrize("use_tls", [True], indirect=True)
-def tes_submit_with_tls(shutdown_only, use_tls):
-    ray.init(num_cpus=2)
+def test_submit_with_tls(shutdown_only, use_tls):
+    ray.init(num_cpus=2, num_gpus=1, resources={"Custom": 1})
 
     @ray.remote
     def f(n):
@@ -79,3 +65,17 @@ def tes_submit_with_tls(shutdown_only, use_tls):
     id1, id2, id3, id4 = a.method._remote(
         args=["test"], kwargs={"b": 2}, num_returns=4)
     assert ray.get([id1, id2, id3, id4]) == [0, 1, "test", 2]
+
+
+@pytest.mark.skipif(
+    sys.platform == "darwin",
+    reason=("Cryptography doesn't install in Mac build pipeline"))
+@pytest.mark.parametrize("use_tls", [True], indirect=True)
+def test_client_connect_to_tls_server(use_tls, init_and_serve):
+    from ray.util.client import ray as ray_client
+    os.environ["RAY_USE_TLS"] = "0"
+    with pytest.raises(ConnectionError):
+        ray_client.connect("localhost:50051")
+
+    os.environ["RAY_USE_TLS"] = "1"
+    ray_client.connect("localhost:50051")
